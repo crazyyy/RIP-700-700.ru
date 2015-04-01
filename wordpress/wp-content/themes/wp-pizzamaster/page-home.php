564d318b48a3a6476ab305a2cfdfea89c5279f5f
DAN-STIX <dan.stix87@gmail.com>
2015-04-01 21:27:02 +0300

    category discount banner

--- wordpress/wp-content/themes/wp-pizzamaster/page-home.php
+++ wordpress/wp-content/themes/wp-pizzamaster/page-home.php
@@ -1,5 +1,4 @@
-<!-- /* Template Name: Home Page Template */  -->
-<?php get_header(); ?>
+<?php /* Template Name: Home Page Template */get_header(); ?>
     <div class="fullslider">
         <div class="fullslider_wrap">
 
