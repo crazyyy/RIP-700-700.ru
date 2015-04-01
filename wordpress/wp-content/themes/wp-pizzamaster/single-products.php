9e593924d47a77c90de6bb53aafb90de02b29a3b
DAN-STIX <dan.stix87@gmail.com>
2015-04-01 17:30:30 +0300

    add product

--- wordpress/wp-content/themes/wp-pizzamaster/single-products.php
+++ wordpress/wp-content/themes/wp-pizzamaster/single-products.php
@@ -1,36 +1,41 @@
 <!-- /* Template Name: Page Template */  -->
 <?php get_header(); ?>
 
-        <div class="main">
+<div class="main">
 
 
-            <div class="content_wrap">
+    <div class="content_wrap">
 
-  <div class="post_wrap">
-              
-        <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+        <div class="post_wrap">
 
-              
-  <h3>title</h3>
+            <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-<?php the_content(); ?>
-<!-- post thumbnail // chek if thumbail exits -->
+
+            <h3><?php the_title(); ?></h3>
+
+            <?php the_content(); ?>
+            <!-- post thumbnail // chek if thumbail exits -->
             <?php if ( has_post_thumbnail()) :?>
-                <a class="single-thumb" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail(); // Fullsize image for the single post ?></a>
+            <a class="single-thumb" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                <?php the_post_thumbnail(); // Fullsize image for the single post ?>
+            </a>
             <?php endif; ?>
             <!-- /post thumbnail -->
 
 
 
-        <?php endwhile; else: // If 404 page error ?>
+            <?php endwhile; else: // If 404 page error ?>
             <h2 class="page-title inner-title"><?php _e( 'Sorry, nothing to display.', 'wpeasy' ); ?></h2>
-        <?php endif; ?>
+            <?php endif; ?>
 
 
-                </div><!-- post_wrap -->
+        </div>
+        <!-- post_wrap -->
 
-            </div><!-- content_wrap -->
-        </div><!-- main -->
+    </div>
+    <!-- content_wrap -->
+</div>
+<!-- main -->
 
 
 <?php get_footer(); ?>
