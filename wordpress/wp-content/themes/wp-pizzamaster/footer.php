c376c9ee03910ade3eda2c1ee43080cee7933208
crazyyy <crazyyy@gmail.com>
2015-04-01 17:19:29 +0300

    popup prerady

--- wordpress/wp-content/themes/wp-pizzamaster/footer.php
+++ wordpress/wp-content/themes/wp-pizzamaster/footer.php
@@ -317,20 +317,15 @@
 
 <?php get_template_part('popup-vote'); ?>
 
+<?php get_template_part('popup-vote-complete'); ?>
 
 
 
 
 
 
-<div class="popup vote_complete">
-    <div class="close"></div>
-    <div class="full_cart_data">
-        <div class="site_close">
-            <h2>Вы уже проголосовали!</h2>
-        </div>
-    </div>
-</div>
+
+
 
 
 
---
+++ wordpress/wp-content/themes/wp-pizzamaster/popup-vote-complete.php
@@ -0,0 +1,8 @@
+<div class="popup vote_complete">
+    <div class="close"></div>
+    <div class="full_cart_data">
+        <div class="site_close">
+            <h2>Вы уже проголосовали!</h2>
+        </div>
+    </div>
+</div>
